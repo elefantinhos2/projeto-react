219e5aa8c85a2baf70bda110883d712fa534fcbd
elefantinhos2 <douglasramires01@gmail.com>
2022-10-04 03:39:22 -0300

    feat:tratando erro  request timeout

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -16,6 +16,9 @@ function Form({listAmounts, setListAmounts}) {
     const [installments, setInstallments]    = useState(0);
     const [mdr, setMdr]                      = useState(0);
 
+    const [inOn, setInOn] = useState(false);
+    const [modal, setModal] = useState("containerModal hidden");
+
     const formSchema = yup.object().shape({
         amount: yup
             .number()
@@ -69,29 +72,63 @@ function Form({listAmounts, setListAmounts}) {
         })
         .catch((err) => {
 
+            if (err.message == "Request failed with status code 408") {
+                showModal()
+                // console.log("TimeOut")
+            }
+
+            console.log(err)
         })
     }
+
+    const showModal = () => {
+        console.log("TimeOut")
+        if( inOn === false ) {
+            setInOn(true);
+            return setModal("containerModal")
+        } else {
+            setInOn(false);
+            return setModal("containerModal hidden")
+        }
+    }
+
+    const removeModal = () => {
+        setInOn(false);
+        return setModal("containerModal hidden")
+    }
+
     
     return (
-        <form className="signupFormAmount" onSubmit={handleSubmit(onRegister)}>
-            <h1>Simule sua Antecipação</h1>
-            <div>
-                <label>Informe o valor da  venda</label>
-                <input type="text" placeholder="R$:" {...register("amount")}/>
-            {errors.amount && <span>{errors.amount.message}</span>}
-            </div>
-            <div>
-                <label>Em quantas parcelas</label>
-                <input type="text" {...register("installments")}/>
-            {errors.installments && <span>{errors.installments.message}</span>}
-            </div>
-            <div>
-                <label>Informe o percentual de MDR</label>
-                <input type="text" {...register("mdr")}/>
-            {errors.mdr && <span>{errors.mdr.message}</span>}
-            </div>
-            <button type="submit">Calcular</button>
-        </form>
+        <section>
+            <section className={modal}>
+                <div className="modal">
+                    <div className="modalHeader">
+                        <h1>Algo deu Errado : (</h1>
+                        <button className="removedModal" onClick={() => removeModal()}>x</button>
+                    </div>
+                    <img src="https://httpcats.com/408.jpg" alt="status erro 408" />
+                </div>
+            </section>
+            <form className="signupFormAmount" onSubmit={handleSubmit(onRegister)}>
+                <h1>Simule sua Antecipação</h1>
+                <div>
+                    <label>Informe o valor da  venda</label>
+                    <input type="text" placeholder="R$:" {...register("amount")}/>
+                {errors.amount && <span>{errors.amount.message}</span>}
+                </div>
+                <div>
+                    <label>Em quantas parcelas</label>
+                    <input type="text" {...register("installments")}/>
+                {errors.installments && <span>{errors.installments.message}</span>}
+                </div>
+                <div>
+                    <label>Informe o percentual de MDR</label>
+                    <input type="text" {...register("mdr")}/>
+                {errors.mdr && <span>{errors.mdr.message}</span>}
+                </div>
+                <button type="submit">Calcular</button>
+            </form>
+        </section>
     )
 }
 
